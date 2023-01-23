47d09d72094d9314a6abeba3a1dcc1d2ecbcab19
Antesser <ahriman.kun@gmail.com>
2023-01-23 22:58:46 +0300

    Changes in html and app

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,11 +1,39 @@
-from flask import request, Flask
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+import psycopg2
+import os
 
+
+load_dotenv()
 
 app = Flask(__name__)
 
+url = os.getenv("DATABASE_URL")
+try:
+    conn = psycopg2.connect(url)
+    print("Подключение установлено")
+    with conn.cursor() as cursor:
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS urls (id bigint PRIMARY KEY GENERATED\
+                ALWAYS AS IDENTITY, name varchar(255), created_at timestamp)"
+        )
+except (Exception, psycopg2.Error) as error:
+    print("Error while connectiong to PostgreSQL", error)
+finally:
+    if conn:
+        cursor.close()
+        conn.close()
 
-@app.route('/', methods=['GET', 'POST'])
+
+@app.route("/")
 def index():
-    if request.method == 'POST':
-        return 'Hello, POST!'
-    return 'Hello, GET!'
+    return render_template("index.html")
+
+
+@app.route("/urls", methods=["GET", "POST"])
+def urls():
+    if request.method == "POST":
+        email = request.form["url"]
+        return render_template("url.html", email=email)
+    else:
+        return render_template("urls.html")
