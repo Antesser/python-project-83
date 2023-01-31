8573061815620630213cefc07cf11aad2c32b201
Antesser <ahriman.kun@gmail.com>
2023-01-31 15:39:28 +0300

    Added '/urls/<id>/checks'

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -31,6 +31,26 @@ finally:
         conn.close()
 
 
+try:
+    conn = psycopg2.connect(url)
+    print("Подключение установлено")
+    with conn.cursor() as cursor:
+        cursor.execute(
+            "CREATE TABLE IF NOT EXISTS url_checks (id bigint,\
+                    url_id bigint GENERATED ALWAYS AS IDENTITY,\
+                        status_code integer, h1 varchar(255),\
+                            title varchar(255), description varchar(255),\
+                                created_at timestamp)"
+        )
+except (Exception, psycopg2.Error) as error:
+    print("Error while connecting to PostgreSQL", error)
+finally:
+    if conn:
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+
 @app.route("/")
 def index():
     messages = get_flashed_messages(with_categories=True)
@@ -43,7 +63,7 @@ def urls():
     if request.method == "GET":
         with psycopg2.connect(url) as conn:
             with conn.cursor(cursor_factory=RealDictCursor) as cursor:
-                cursor.execute("SELECT * FROM urls")
+                cursor.execute("SELECT * FROM urls ORDER BY id DESC")
                 list_of_urls = cursor.fetchall()
                 return render_template("urls.html", list_of_urls=list_of_urls)
     elif request.method == "POST":
@@ -93,3 +113,25 @@ def url_id(id):
                 date = i.get("created_at").strftime("%Y-%m-%d")
         return render_template("url.html", url=fetched_url, date=date, id=id,
                                messages=messages)
+
+
+@app.route("/urls/<id>/checks", methods=["POST"])
+def url_id_check(id):
+    current_date = datetime.now()
+    with psycopg2.connect(url) as conn:
+        with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+            cursor.execute(
+                "INSERT INTO url_checks (id, created_at)\
+                            VALUES (%s,%s)",
+                (id, current_date),
+            )
+            cursor.execute("SELECT url_id, created_at FROM url_checks\
+                WHERE id = %s ORDER BY url_id DESC", (id,))
+            test_results = cursor.fetchall()
+            for i in test_results:
+                url_id = i.get("url_id")
+                check_date = i.get("created_at").strftime("%Y-%m-%d")
+        flash("Страница успешно проверена", "success")
+        return render_template("url.html", url_id=url_id,
+                               check_date=check_date,
+                               test_results=test_results)
