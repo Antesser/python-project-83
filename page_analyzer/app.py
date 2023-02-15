6df609949cf001bfcd28da69e661c77292ccdb6d
Antesser <ahriman.kun@gmail.com>
2023-02-15 18:05:53 +0300

    Last changes with dates

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -27,8 +27,7 @@ try:
         cursor.execute(
             """CREATE TABLE IF NOT EXISTS url_checks (id bigint PRIMARY KEY
                 GENERATED ALWAYS AS IDENTITY,
-                    url_id bigint,
-                        status_code integer, h1 varchar(255),
+                    url_id bigint, status_code integer, h1 varchar(255),
                             title varchar(255), description varchar(255),
                                 created_at timestamp)"""
         )
